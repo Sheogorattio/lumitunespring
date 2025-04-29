2b2c9292a5f383386e7b2d3e1fea862a861aed62
Sheogorattio <No.passion.there.is.serenity@gmail.com>
2025-04-29 09:25:07 +0300

    Update LumitunespringApplication.java

--- src/main/java/com/blacksabbath/lumitunespring/LumitunespringApplication.java
+++ src/main/java/com/blacksabbath/lumitunespring/LumitunespringApplication.java
@@ -14,12 +14,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class LumitunespringApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		/*Dotenv dotenv = Dotenv.load();
 		
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         System.setProperty("DB_ADDRESS", dotenv.get("DB_ADDRESS"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));*/
+		
+		System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+	    System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+	    System.setProperty("DB_ADDRESS", System.getenv("DB_ADDRESS"));
+	    System.setProperty("DB_NAME", System.getenv("DB_NAME"));
         
 		SpringApplication.run(LumitunespringApplication.class, args);
 	}
