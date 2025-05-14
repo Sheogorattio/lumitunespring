25bcdefc6288ec3287dda36257f02f3b5309f2f6
Sheogorattio <No.passion.there.is.serenity@gmail.com>
2025-05-14 23:28:28 +0300

    .

--- src/main/java/com/blacksabbath/lumitunespring/misc/IsDev.java
+++ src/main/java/com/blacksabbath/lumitunespring/misc/IsDev.java
@@ -1,24 +1,10 @@
 package com.blacksabbath.lumitunespring.misc;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class IsDev {
-	private static Dotenv dotenv = Dotenv.load();
 	public IsDev() {
-		try {
-			dotenv = Dotenv.load();
-			if(dotenv != null)
-				System.out.println("Dotenv is not null");
-			else
-				System.out.println("Dotenv is null");
-		}
-		catch( Exception e){
-			System.out.println("Dotenv is null");
-		}
+
 	}
 	public static String getEnv(String key) {
 		return System.getenv(key);
-		//return dotenv.get(key);
-		//return (dotenv != null && dotenv.get(key) != null) ? dotenv.get(key) : System.getenv(key);
 	}
 }
