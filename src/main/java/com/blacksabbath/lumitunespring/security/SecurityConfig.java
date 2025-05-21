315193e9279205165a6afbb06230a73c33254a86
Sheogorattio <No.passion.there.is.serenity@gmail.com>
2025-05-21 03:23:57 +0300

    removed "/" from "https://lumitune.vercel.app/" in cors configuration

--- src/main/java/com/blacksabbath/lumitunespring/security/SecurityConfig.java
+++ src/main/java/com/blacksabbath/lumitunespring/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOriginPatterns(List.of("https://lumitune.vercel.app/", "http://127.0.0.1:5500", "http://localhost:5500"));
+		configuration.setAllowedOriginPatterns(List.of("https://lumitune.vercel.app", "http://127.0.0.1:5500", "http://localhost:5500"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
